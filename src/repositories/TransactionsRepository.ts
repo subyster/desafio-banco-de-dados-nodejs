fc79ad24f517e24d7423296957311f6f399b65b5
subyster <arthur.amaral1@gmail.com>
2020-04-19 21:39:55 -0300

    desafio completo

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,31 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const totalIncome = transactions.reduce(
+      (total, transaction) =>
+        transaction.type === 'income'
+          ? Number(total) + Number(transaction.value)
+          : Number(total),
+      0,
+    );
+
+    const totalOutcome = transactions.reduce(
+      (total, transaction) =>
+        transaction.type === 'outcome'
+          ? Number(total) + Number(transaction.value)
+          : Number(total),
+      0,
+    );
+
+    const totalBalance = totalIncome - totalOutcome;
+    const balance: Balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalBalance,
+    };
+
+    return balance;
   }
 }
 
